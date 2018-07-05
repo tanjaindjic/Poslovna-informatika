c8b11cfb22baa9401aab2fb83367a1f59a925a2b
tanjaindjic <mali.patuljko@gmail.com>
2018-07-05 12:06:18 +0200

    init

---
+++ src/main/java/com/poslovna/poslovna/PoslovnaApplication.java
@@ -0,0 +1,12 @@
+package com.poslovna.poslovna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoslovnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoslovnaApplication.class, args);
+	}
+}
