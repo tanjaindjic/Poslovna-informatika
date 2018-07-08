5fc2797435bacc5b787b00d5dd96796c92d26ba6
tanjaindjic <mali.patuljko@gmail.com>
2018-07-09 01:11:34 +0200

    prikaz naloga, posle checkboxa ne izgleda lepo

--- src/main/resources/static/appParts/klijentIzvodiComponent/klijentIzvodiController.js
+++ src/main/resources/static/appParts/klijentIzvodiComponent/klijentIzvodiController.js
@@ -2,8 +2,18 @@ mainModule.controller('klijentIzvodiController', ['$scope', '$window', 'userServ
     function($scope, $window, userService, $location, $http){
 
         $scope.logovaniKorisnik = {};
-        $scope.odabranRacun = "";
-        $scope.
+        $scope.nalozi = [];
+        $scope.odabranRacun;
+
+        var setNalozi = function(){
+            var i = 0;
+            for(i = 0; i < $scope.sviNalozi.length; i++)
+                if($scope.sviNalozi[i].racunNalogodavca==$scope.odabranRacun || $scope.sviNalozi[i].racunPrimaoca==$scope.odabranRacun)
+                    $scope.nalozi.push($scope.sviNalozi[i]);
+
+        }
+
+
         $scope.initKlijent = function(){
             $scope.logovaniKorisnik = userService.parsirajToken();
 
@@ -31,8 +41,11 @@ mainModule.controller('klijentIzvodiController', ['$scope', '$window', 'userServ
                 if(response.data ==null)
                     $location.path('/login');
 
-                $scope.nalozi = response.data;
-                console.log($scope.nalozi.length)
+                $scope.sviNalozi = response.data;
+                $scope.odabranRacun = $scope.klijent.racuni[0].brojRacuna;
+                setNalozi();
+
+                console.log($scope.odabranRacun)
 
             }, function errorCallback(response) {
                 alert("Error occured check connection");
@@ -43,6 +56,12 @@ mainModule.controller('klijentIzvodiController', ['$scope', '$window', 'userServ
 
         $scope.initKlijent();
 
+        $scope.odabirRacuna = function(odabranRacun){
+            $scope.odabranRacun = odabranRacun;
+            $scope.nalozi = [];
+            setNalozi();
+        }
+
         $scope.profil = function(){
             var tempUser = parsirajToken();
             if(tempUser.uloga === 'KLIJENT'){
@@ -50,6 +69,42 @@ mainModule.controller('klijentIzvodiController', ['$scope', '$window', 'userServ
             }else{
                  $window.location('/home');
             }
+        }
+
+        $scope.samoUplate = function(){
+            var checkBox = document.getElementById("upl");
+            if (checkBox.checked == true){
+                var i = 0;
+                for( i = 0; i < $scope.nalozi.length; i++){
+                    if($scope.nalozi[i].racunPrimaoca == $scope.odabranRacun)
+                        document.getElementById($scope.nalozi[i].id).style.display = 'block';
+                    else document.getElementById($scope.nalozi[i].id).style.display = 'none';
+                }
+            }else {
+                var i = 0;
+                for( i = 0; i < $scope.nalozi.length; i++){
+                    document.getElementById($scope.nalozi[i].id).style.display = 'block';
+                }
+            }
+
+        }
+
+        $scope.samoIsplate = function(){
+            var checkBox = document.getElementById("ispl");
+            if (checkBox.checked == true){
+                var i = 0;
+                for( i = 0; i < $scope.nalozi.length; i++){
+                    if($scope.nalozi[i].racunNalogodavca == $scope.odabranRacun)
+                        document.getElementById($scope.nalozi[i].id).style.display = 'block';
+                    else document.getElementById($scope.nalozi[i].id).style.display = 'none';
+                }
+            }else{
+                var i = 0;
+                for( i = 0; i < $scope.nalozi.length; i++){
+                    document.getElementById($scope.nalozi[i].id).style.display = 'block';
+                }
+            }
+
         }
     }
 ]);
